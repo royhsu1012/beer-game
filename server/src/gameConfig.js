fdb61a4533a8f11b7cd6c39ac2e9d6a15eff62d4
royhsu <royhsu1012@gmail.com>
2026-06-25 00:50:49 +0800

    add gameConfig.js

---
+++ server/src/gameConfig.js
@@ -0,0 +1,19 @@
+const GAME_CONFIG = {
+  TOTAL_WEEKS: 20,
+  LEAD_TIME: 2,
+  INITIAL_INVENTORY: 12,
+  INITIAL_PIPELINE: [4, 4],
+  HOLDING_COST: 0.50,
+  SHORTAGE_COST: 1.00,
+  ROUND_TIME_SECONDS: 60,
+  ROLES: ["retailer", "wholesaler", "distributor", "manufacturer"],
+  DEMAND_CURVE: [4,4,4,4,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8],
+  EXCELLENT_THRESHOLD: {
+    retailer: 150,
+    wholesaler: 200,
+    distributor: 250,
+    manufacturer: 300,
+    team: 700
+  }
+}
+module.exports = GAME_CONFIG
